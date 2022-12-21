e60bd279071802ccb5a285a4d37cfc91c71a06c0
satalurius <max99810@gmail.com>
2022-12-21 21:03:39 +0300

    Интерфейс удаления пользователей

--- VanKassa.Backend.Core/Services/Interface/IEmployeesService.cs
+++ VanKassa.Backend.Core/Services/Interface/IEmployeesService.cs
@@ -5,5 +5,6 @@ namespace VanKassa.Backend.Core.Services.Interface;
 public interface IEmployeesService
 {
     Task<IEnumerable<EmployeesDbDto>?> GetEmployeesAsync();
+    Task DeleteEmployeesAsync(IEnumerable<int> deletedIds);
     Task<PageEmployeesDto?> GetEmployeesWithFiltersAsync(EmployeesPageParameters parameters);
 }
