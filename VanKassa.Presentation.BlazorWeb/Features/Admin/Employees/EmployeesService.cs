9e11a36cafa4867991dc4d938dca1e313f5da64c
cherenkov.maxim <>
2023-01-18 16:05:59 +0300

    Delete employees

--- VanKassa.Presentation.BlazorWeb/Features/Admin/Employees/EmployeesService.cs
+++ VanKassa.Presentation.BlazorWeb/Features/Admin/Employees/EmployeesService.cs
@@ -3,7 +3,7 @@ using System.Text;
 using Newtonsoft.Json;
 using VanKassa.Domain.Dtos;
 using Microsoft.AspNetCore.WebUtilities;
-
+using VanKassa.Presentation.BlazorWeb.Features.Shared.Exceptions;
 
 namespace VanKassa.Presentation.BlazorWeb.Features.Admin.Employees;
 
@@ -11,12 +11,13 @@ public class EmployeesService
 {
     private readonly HttpClient httpClient;
 
-    // TODO: Брать из настроек
-    private static readonly string WebApiAddress = "http://localhost:5289/api/employees";
-
-    public EmployeesService(HttpClient httpClient)
+    private readonly string webApiAddress;
+    
+    public EmployeesService(HttpClient httpClient, IConfiguration configuration)
     {
         this.httpClient = httpClient;
+        webApiAddress = configuration.GetConnectionString("ApiAddress") 
+                        ?? throw new ArgumentNullException("Api address path does not exist");
     }
 
     public async Task<PageEmployeesDto?> GetEmployeesAsync(EmployeesPageParameters pageParameters)
@@ -32,15 +33,33 @@ public class EmployeesService
                 ["filter_text"] = pageParameters.FilterText
             };
 
-            var uri = QueryHelpers.AddQueryString(WebApiAddress + "/all", query);
+            var uri = QueryHelpers.AddQueryString(webApiAddress + "/all", query);
 
             var pageEmp = await httpClient.GetFromJsonAsync<PageEmployeesDto>(uri);
 
             return pageEmp;
         }
-        catch (HttpRequestException ex)
+        catch (HttpRequestException)
         {
             return null;
+        }
+    }
+
+    public async Task DeleteEmployeesAsync(IEnumerable<int> deleteIds)
+    {
+        try
+        {
+            var uri = $"{webApiAddress}/delete";
+
+            var json = JsonConvert.SerializeObject(deleteIds);
+
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            await httpClient.PostAsync(uri, httpContent);
+        }
+        catch (HttpRequestException)
+        {
+            throw new QueryException();
         }
     }
 }
