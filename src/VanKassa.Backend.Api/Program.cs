dd3067fbc765b4be11d214718edecb815c50fff4
satalurius <max99810@gmail.com>
2023-05-20 10:18:46 +0300

    add swagger auth

--- src/VanKassa.Backend.Api/Program.cs
+++ src/VanKassa.Backend.Api/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 using VanKassa.Backend.Api.Extensions;
 using VanKassa.Backend.Api.Middlewares;
 using VanKassa.Domain.Constants;
@@ -22,7 +23,34 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
 {
- options.UseInlineDefinitionsForEnums();   
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Admin panel API for Cafe", Version = "v1" });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+    options.UseInlineDefinitionsForEnums();
 });
 
 builder.Services.AddRazorTemplating();
